ca1444cdde76b5f6f7885e4d72710e06e571a687
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2026-04-09 08:23:42 +0200

    consent implementation

--- internal/handlers/consent.go
+++ internal/handlers/consent.go
@@ -1,12 +1,37 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 )
 
 func (a *App) Consent(w http.ResponseWriter, r *http.Request) {
+	clientID := r.URL.Query().Get("client_id")
+	responseType := r.URL.Query().Get("response_type")
+	redirectURI := r.URL.Query().Get("redirect_uri")
 	scope := r.URL.Query().Get("scope")
+	state := r.URL.Query().Get("state")
+
+	if r.Method == "POST" {
+		userID, err := GetUserFromRequest(r, a.Sessions)
+		if err != nil {
+			loginURI := CreateURI("/login", clientID, responseType, redirectURI, scope, state)
+			http.Redirect(w, r, loginURI, http.StatusFound)
+			return
+		}
+
+		if r.FormValue("scopes") == scope {
+			a.Consents[userID] = Consent{
+				ClientID: clientID,
+				Scope:    scope,
+			}
+			fmt.Println(a)
+		}
+
+	}
+
+	consentURI := CreateURI("/consent", clientID, responseType, redirectURI, scope, state)
 
 	template, err := template.ParseFiles("templates/consent.html")
 	if err != nil {
@@ -14,5 +39,10 @@ func (a *App) Consent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	template.Execute(w, scope)
+	vars := struct {
+		FormAction string
+		Scopes     string
+	}{consentURI, scope}
+
+	template.Execute(w, vars)
 }
